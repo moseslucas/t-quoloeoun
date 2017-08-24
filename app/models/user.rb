b33d2ce6e5ddf0d09e2d8947a42ea6bc91091a57
moseslucas <mo@moseslucas.com>
2017-08-24 09:54:06 +0800

    Generated User Devise with backend validation for fields, generated views to be enhanced

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, :encrypted_password, presence: true
+  validates :encrypted_password, length: {minimum: 6}
+  validates :email, :email_format => { :message => 'Invalid Email Address' }
+end
