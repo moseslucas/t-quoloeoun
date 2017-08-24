d8ee0fec5d63e8af04745d8f69252b816551949a
moseslucas <mo@moseslucas.com>
2017-08-24 15:47:42 +0800

    Added layout for addition info in profiles

--- app/assets/javascripts/components/lagayan.es6.jsx
+++ app/assets/javascripts/components/lagayan.es6.jsx
@@ -1,8 +1,25 @@
 class Lagayan extends React.Component {
+  renderAdditionalInfo(){
+    return(
+      <div>
+        <h3 className="text-center">Profile</h3>
+        <ul className="list-group">
+          <li className="list-group-item text-primary"><label>lukeses09@gmail.com</label></li>
+          <li className="list-group-item"><label>Total Tweets: </label> <text className="pull-right">4</text></li>
+          <li className="list-group-item"><label>Following: </label> <text className="pull-right">4</text></li>
+          <li className="list-group-item"><label>Followers: </label> <text className="pull-right">4</text></li>
+          <li className="list-group-item"><label>Member Since: </label> <text className="pull-right">Aug 20, 2017</text></li>
+        </ul>
+      </div>
+    )
+  }
+
   render () {
     return(
       <div className="row">
-        <div className="col-sm-3"></div>
+        <div className="col-sm-3">
+          {this.renderAdditionalInfo()}
+        </div>
         <div className="col-sm-6">
           {this.props.children}
         </div>
