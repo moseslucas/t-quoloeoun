b9f30604f6d6d33de685d3c2b5b1817b3f9b17bb
moseslucas <mo@moseslucas.com>
2017-08-24 16:30:50 +0800

    Added Member Since to AdditionInfoToProfile

--- app/assets/javascripts/components/lagayan.es6.jsx
+++ app/assets/javascripts/components/lagayan.es6.jsx
@@ -8,12 +8,18 @@ class Lagayan extends React.Component {
         <ul className="list-group">
           <li className="list-group-item text-primary"><label>{this.props.user.email}</label></li>
           <li className="list-group-item"><label>Total Tweets: </label> <text className="pull-right">{p.total_twits}</text></li>
-          <li className="list-group-item"><label>Following: </label> <text className="pull-right">4</text></li>
-          <li className="list-group-item"><label>Followers: </label> <text className="pull-right">4</text></li>
-          <li className="list-group-item"><label>Member Since: </label> <text className="pull-right">Aug 20, 2017</text></li>
+          <li className="list-group-item"><label>Following: </label> <text className="pull-right">0</text></li>
+          <li className="list-group-item"><label>Followers: </label> <text className="pull-right">0</text></li>
+          <li className="list-group-item"><label>Member Since: </label> <text className="pull-right">{this.format_date(p.user.created_at)}</text></li>
         </ul>
       </div>
     )
+  }
+
+  format_date(created_at){
+    let since = new Date(created_at)
+    since = `${since.getMonth()+1}/${since.getDate()}/${since.getFullYear()}`
+    return since
   }
 
   render () {
